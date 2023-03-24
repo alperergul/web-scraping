78f863bb228a5c2091516d052fe3121c790d0a98
alperergul <alper.ergul.97@gmail.com>
2023-03-25 01:09:20 +0300

    fixed bug

--- project1/main.py
+++ project1/main.py
@@ -12,57 +12,49 @@ options.headless = False
 
 web = "https://www.audible.com/adblbestsellers?ref=a_search_t1_navTop_pl0cg1c0r0&pf_rd_p=8a113f1a-dc38-418d-b671-3cca04245da5&pf_rd_r=B2S0T9ACNZNTS4BRF26A&pageLoadId=LGzChPKuzMRIStRw&creativeId=1642b4d1-12f3-4375-98fa-4938afc1cedc"
 path = "C:/Users/alper/AlperProjects/chromedriver_win32/chromedriver.exe"
-driver = webdriver.Chrome(path, options=options)
+driver = webdriver.Chrome(path)
 driver.get(web)
-# driver.maximize_window()
+driver.maximize_window()
 
-
-# pagination
 pagination = driver.find_element_by_xpath(
     '//ul[contains(@class, "pagingElements")]')
 pages = pagination.find_elements_by_tag_name('li')
 last_page = int(pages[-2].text)
 
-
-current_page = 1
-
 book_title = []
 book_author = []
 book_length = []
+current_page = 1
 
 while current_page <= last_page:
-
+    # Implicit Wait
     time.sleep(2)
-    # container = WebDriverWait(driver, 5).until(
-    #     EC.presence_of_element_located((By.CLASS_NAME, 'adbl-impression-container')))
-    container = driver.find_element_by_class_name('adbl-impression-container')
-    # products = WebDriverWait(driver, 5).until(
-    #     EC.presence_of_all_elements_located((By.XPATH, './li')))
-
-    products = container.find_elements_by_xpath(
-        './/li[contains(@class, "productListItem")]')
+    # Explicit Wait
+    # container = driver.find_element_by_class_name('adbl-impression-container ')
+    container = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.CLASS_NAME, 'adbl-impression-container ')))
+    # products = container.find_elements_by_xpath('.//li[contains(@class, "productListItem")]')
+    products = WebDriverWait(container, 5).until(EC.presence_of_all_elements_located(
+        (By.XPATH, './/li[contains(@class, "productListItem")]')))
 
     for product in products:
         book_title.append(product.find_element_by_xpath(
-            './/h3[contains(@class,"bc-heading")]').text)
+            './/h3[contains(@class, "bc-heading")]').text)
         book_author.append(product.find_element_by_xpath(
-            './/li[contains(@class,"authorLabel")]').text)
+            './/li[contains(@class, "authorLabel")]').text)
         book_length.append(product.find_element_by_xpath(
-            './/li[contains(@class,"runtimeLabel")]').text)
+            './/li[contains(@class, "runtimeLabel")]').text)
 
-    current_page += 1
+    current_page = current_page + 1
     try:
-
-        nextPage = driver.find_element_by_xpath(
-            '//span[contains(@class, "nextButton")]')
-        nextPage.click()
+        next_page = driver.find_element_by_xpath(
+            './/span[contains(@class , "nextButton")]')
+        next_page.click()
     except:
         pass
 
 driver.quit()
 
-
-df = pd.DataFrame(
-    {'title': book_title, "author": book_author, "length": book_length})
-
-df.to_csv('BookList_pagination.csv', index=False)
+df_books = pd.DataFrame(
+    {'title': book_title, 'author': book_author, 'length': book_length})
+df_books.to_csv('books_pagination.csv', index=False)
